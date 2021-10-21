9e3dce39ed0d798f6a95a6c99834aec3c4f6adb9
Jonas Metzger <metzgerj@stanford.edu>
2021-10-20 19:20:29 -0700

    fixed oracle

--- oracle.py
+++ oracle.py
@@ -1,30 +1,27 @@
 import opencontracts
 import urllib.request, certifi, ssl
 
-def main():
-    with opencontracts.enclave_backend() as enclave:
 
-      enclave.print("Enclave says hello!")
+with opencontracts.enclave_backend() as enclave:
 
-      # you need to call this before trying to connect to a domain
-      enclave.open_up_domain("en.wikipedia.org")
+  enclave.print("Enclave says hello!")
 
-      # use e.g. urllib to perform a secure https requests as you would anywhere
-      ssl_context = ssl.create_default_context(cafile=certifi.where()) # checks certificate validity relative to Mozillas CA store
-      with urllib.request.urlopen('https://en.wikipedia.org/wiki/Vitalik_Buterin', context=ssl_context) as f:
-        html = f.read().decode('utf-8')
-        enclave.print(html[:400])
+  # you need to call this before trying to connect to a domain
+  enclave.open_up_domain("en.wikipedia.org")
 
-      # starts an interactive chrome session at http://<EC2_IP>:14500
-      # (en.wikipedia.org is still the only open domain)
-      html = enclave.interactive_session(url='en.wikipedia.org', instructions="Do the thing then push the button.", tcp_port=14500)
-      enclave.print(html[:200])
+  # use e.g. urllib to perform a secure https requests as you would anywhere
+  ssl_context = ssl.create_default_context(cafile=certifi.where()) # checks certificate validity relative to Mozillas CA store
+  with urllib.request.urlopen('https://en.wikipedia.org/wiki/Vitalik_Buterin', context=ssl_context) as f:
+    html = f.read().decode('utf-8')
+    enclave.print(html[:400])
 
-      pw = enclave.user_input("Gimme ya password")
-      enclave.print(f"HAHA! Your password is {pw}."
-      enclave.submit(html[:200], pw, types=("string", "string"))
+  # starts an interactive chrome session at http://<EC2_IP>:14500
+  # (en.wikipedia.org is still the only open domain)
+  html = enclave.interactive_session(url='en.wikipedia.org', instructions="Do the thing then push the button.", tcp_port=14500)
+  enclave.print(html[:200])
 
+  pw = enclave.user_input("Gimme ya password")
+  enclave.print(f"HAHA! Your password is {pw}.")
+  enclave.submit(html[:200], pw, types=("string", "string"))
 
-if __name__ == '__main__':
-    main()
 
