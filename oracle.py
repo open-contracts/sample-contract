51494852ad901567858cfc4ff1270457d4225861
Krishnan Srinivasan <krshna@stanford.edu>
2021-10-27 19:56:46 +0000

    updated oracle

--- oracle.py
+++ oracle.py
@@ -13,7 +13,7 @@ with opencontracts.enclave_backend() as enclave:
   # use e.g. urllib to perform a secure https requests as you would anywhere
   ssl_context = ssl.create_default_context(cafile=certifi.where()) # checks certificate validity relative to Mozillas CA store
 
-  ret = subprocess.check_output('ls', shell=True, cwd='/')
+  ret = subprocess.check_output('ls', shell=True, cwd='/').decode('utf-8')
   print(ret)
   enclave.print(ret)
 
